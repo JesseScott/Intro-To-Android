5b8f699c2cbd155e397870afbc34049438f665e4
Jesse Scott <jessecolinscott@gmail.com>
2014-02-11 19:51:47 -0800

    Lesson 2 - Button Navigation

---
+++ Lessons/Lesson 2/AndroidButtonNavigation/src/com/example/androidbuttonnavigation/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.androidbuttonnavigation;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Button button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent("android.intent.action.SECOND"));
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
